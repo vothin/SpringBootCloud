dfcb27ac3e461bd0f6723a894c5cb9bd75055ca7
vothin <zy757161350@qq.com>
2020-07-23 11:21:54 +0800

    读取配置中心文件

--- config-client/src/main/java/com/zy/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/zy/configclient/ConfigClientApplication.java
@@ -3,6 +3,7 @@ package com.zy.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ public class ConfigClientApplication {
 
 
 	@Value("${foo}")
-    String foo;
+    private String foo;
 
 
 	@RequestMapping(value = "/hi")
